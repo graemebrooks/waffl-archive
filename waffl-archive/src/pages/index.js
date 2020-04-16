44060480ff5eef083fa0f2655a446a5b04d4fcb7
Kelso <graemebrooks@gmail.com>
2020-04-16 06:53:30 -0500

    adds index page content

--- waffl-archive/src/pages/index.js
+++ waffl-archive/src/pages/index.js
@@ -1,15 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
+// components
 import Layout from '../components/layout/layout';
 
-import wafflLogo from '../../static/waffl-logos/waffl-14.png';
+// static assets
+import wafflLogo from '../../static/waffl-logos/wafflArchive.png';
 
+// styles
 import indexStyles from './indexPage.module.scss';
+const Div = styled.div`
+	height: 90vh;
+	width: 100vw;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	h2 {
+		color: orange;
+	}
+
+	.logoBox {
+		display: flex;
+
+		h1 {
+			padding: 1rem;
+			font-size: 5rem;
+			align-content: center;
+			justify-content: center;
+		}
+	}
+
+	.introText {
+		width: 60vw;
+	}
+`;
 
 export default () => (
 	<Layout>
-		<h1 className={indexStyles.title}>WAFFL Archive</h1>
-		<h2>The digital home of the Westwood Affiliated Fantasy Football League</h2>
-		<img src={wafflLogo} className={indexStyles.logo} alt="WAFFL Logo" />
+		<Div>
+			<div className="logoBox">
+				<img src={wafflLogo} className={indexStyles.logo} alt="WAFFL Logo" />
+			</div>
+			<h2>The digital home of the Westwood Affiliated Fantasy Football League</h2>
+			<p className="introText">
+				The Westwood Affiliated Fantasy Football League or "WAFFL" was founded in 2013. It's founding members
+				included Kyle Harris, Ashley Melero, Jacob Phillips, Kelso Brooks, Dylon Hopp, Matt Canik, Chris Canik,
+				Matt Lessig, Patrick Sladek, and Cody Simms. Since it's founding the league has also welcomed Shawn
+				Webster, Sean Van Hoose, Pooja Shetty, Matt Melleen, Trevor Gunn, and Tina Till.
+			</p>
+		</Div>
 	</Layout>
 );
