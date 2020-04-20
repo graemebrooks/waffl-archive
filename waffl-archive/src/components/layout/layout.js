5b3fbfb58716e3aeaddf4b9f77e55dd2b9a0bc94
Kelso <graemebrooks@gmail.com>
2020-04-20 04:33:53 -0500

    configures favicon

--- waffl-archive/src/components/layout/layout.js
+++ waffl-archive/src/components/layout/layout.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import favicon from '../../../static/favicon-32x32.png';
 
 //styles
 import '../../styles/index.scss';
@@ -11,6 +14,19 @@ import Header from '../header/header';
 const Layout = (props) => {
 	return (
 		<div className={layoutStyles.index}>
+			<Helmet
+				meta={[
+					{
+						name: 'description',
+						content: 'WAFFL Archive'
+					},
+					{
+						name: 'keywords',
+						content: 'football, fantasy'
+					}
+				]}
+				link={[ { rel: 'icon', type: 'image/png', sizes: '32x32', href: `${favicon}` } ]}
+			/>
 			<Header />
 			<div className={layoutStyles.container}>
 				<div className={layoutStyles.content}>{props.children}</div>
