20a4901812b29c0a739f43d28d9b1095cd92e680
Kelso <graemebrooks@gmail.com>
2020-04-24 10:51:16 -0500

    converts to index scss module

--- waffl-archive/src/pages/index.js
+++ waffl-archive/src/pages/index.js
@@ -9,53 +9,53 @@ import Layout from '../components/layout/layout';
 import wafflLogo from '../../static/waffl-logos/wafflArchive.png';
 
 // styles
-// import indexStyles from './indexPage.module.scss';
-const Div = styled.div`
-	height: 90vh;
-	width: 100vw;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+import indexStyles from './indexPage.module.scss';
+// const Div = styled.div`
+// 	height: 90vh;
+// 	width: 100vw;
+// 	display: flex;
+// 	flex-direction: column;
+// 	justify-content: center;
+// 	align-items: center;
 
-	h2 {
-		color: #17b978;
-	}
+// 	h2 {
+// 		color: #17b978;
+// 	}
 
-	img {
-		height: 400px;
-	}
+// 	img {
+// 		height: 400px;
+// 	}
 
-	.logoBox {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+// 	.logoBox {
+// 		display: flex;
+// 		flex-direction: column;
+// 		justify-content: center;
+// 		align-items: center;
 
-		padding: 1.5rem;
+// 		padding: 1.5rem;
 
-		border-radius: .5rem;
-		background: #2d4059;
-	}
+// 		border-radius: .5rem;
+// 		background: #2d4059;
+// 	}
 
-	.introText {
-		width: 60vw;
-	}
-`;
+// 	.introText {
+// 		width: 60vw;
+// 	}
+// `;
 
 export default () => (
 	<Layout>
-		<Div>
-			<motion.div className="logoBox">
-				<img src={wafflLogo} alt="WAFFL Logo" />
+		<div className={indexStyles.content}>
+			<motion.div className={indexStyles.logoBox} animate={{ scale: 1.1 }} transition={{ duration: 0.3 }}>
+				<img src={wafflLogo} className={indexStyles.logo} alt="WAFFL Logo" />
 				<h2>The digital home of the Westwood Affiliated Fantasy Football League</h2>
-				<p className="introText">
+				<p className={indexStyles.introText}>
 					The Westwood Affiliated Fantasy Football League or "WAFFL" was founded in 2013. It's founding
 					members included Kyle Harris, Ashley Melero, Jacob Phillips, Kelso Brooks, Dylon Hopp, Matt Canik,
 					Chris Canik, Matt Lessig, Patrick Sladek, and Cody Simms. Since it's founding the league has also
 					welcomed Shawn Webster, Sean Van Hoose, Pooja Shetty, Matt Melleen, Trevor Gunn, and Tina Till.
 				</p>
 			</motion.div>
-		</Div>
+		</div>
 	</Layout>
 );
