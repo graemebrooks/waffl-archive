6fd82c77ed8c4fe851e5dd9c56054b3458345e26
Kelso <graemebrooks@gmail.com>
2020-06-29 18:36:53 -0500

    add points against to team record chart

--- waffl-archive/src/components/teamRecordChart/teamRecordChart.js
+++ waffl-archive/src/components/teamRecordChart/teamRecordChart.js
@@ -37,37 +37,44 @@ const TeamRecordChart = ({ teamRecordData, team, primaryColor, secondaryColor })
 	const data = [
 		{
 			season: '2013',
-			'Points Scored': teamRecordData[convertName(team)].seasons[0].pointsFor / 14,
+			'Points Per Game': teamRecordData[convertName(team)].seasons[0].pointsFor / 14,
+			'Points Against Per Game': teamRecordData[convertName(team)].seasons[0].pointsAgainst / 14,
 			Wins: teamRecordData[convertName(team)].seasons[0].wins
 		},
 		{
 			season: '2014',
-			'Points Scored': teamRecordData[convertName(team)].seasons[1].pointsFor / 14,
+			'Points Per Game': teamRecordData[convertName(team)].seasons[1].pointsFor / 14,
+			'Points Against Per Game': teamRecordData[convertName(team)].seasons[1].pointsAgainst / 14,
 			Wins: teamRecordData[convertName(team)].seasons[1].wins
 		},
 		{
 			season: '2015',
-			'Points Scored': teamRecordData[convertName(team)].seasons[2].pointsFor / 14,
+			'Points Per Game': teamRecordData[convertName(team)].seasons[2].pointsFor / 14,
+			'Points Against Per Game': teamRecordData[convertName(team)].seasons[2].pointsAgainst / 14,
 			Wins: teamRecordData[convertName(team)].seasons[2].wins
 		},
 		{
 			season: '2016',
-			'Points Scored': teamRecordData[convertName(team)].seasons[3].pointsFor / 14,
+			'Points Per Game': teamRecordData[convertName(team)].seasons[3].pointsFor / 14,
+			'Points Against Per Game': teamRecordData[convertName(team)].seasons[3].pointsAgainst / 14,
 			Wins: teamRecordData[convertName(team)].seasons[3].wins
 		},
 		{
 			season: '2017',
-			'Points Scored': teamRecordData[convertName(team)].seasons[4].pointsFor / 14,
+			'Points Per Game': teamRecordData[convertName(team)].seasons[4].pointsFor / 14,
+			'Points Against Per Game': teamRecordData[convertName(team)].seasons[4].pointsAgainst / 14,
 			Wins: teamRecordData[convertName(team)].seasons[4].wins
 		},
 		{
 			season: '2018',
-			'Points Scored': teamRecordData[convertName(team)].seasons[5].pointsFor / 14,
+			'Points Per Game': teamRecordData[convertName(team)].seasons[5].pointsFor / 14,
+			'Points Against Per Game': teamRecordData[convertName(team)].seasons[5].pointsAgainst / 14,
 			Wins: teamRecordData[convertName(team)].seasons[5].wins
 		},
 		{
 			season: '2019',
-			'Points Scored': teamRecordData[convertName(team)].seasons[6].pointsFor / 14,
+			'Points Per Game': teamRecordData[convertName(team)].seasons[6].pointsFor / 14,
+			'Points Against Per Game': teamRecordData[convertName(team)].seasons[6].pointsAgainst / 14,
 			Wins: teamRecordData[convertName(team)].seasons[6].wins
 		}
 	];
@@ -79,7 +86,7 @@ const TeamRecordChart = ({ teamRecordData, team, primaryColor, secondaryColor })
 					<XAxis dataKey="season" stroke="#000000" />
 					<YAxis type="number" domain={[ 75, 165 ]} orientation="right" yAxisId="left" stroke="#000000" />
 					<YAxis type="number" domain={[ 0, 14 ]} orientation="left" yAxisId="right" stroke="#000000" />
-					<Tooltip />
+					<Tooltip formatter={(value, name, props) => [ `${Math.floor(value)}`, name ]} />
 					<Legend />
 					<CartesianGrid stroke={primaryColor} fill="#FFFFFF" />
 					<ReferenceLine y={130.7} stroke="green" strokeDasharray="3 3" yAxisId="left">
@@ -94,7 +101,8 @@ const TeamRecordChart = ({ teamRecordData, team, primaryColor, secondaryColor })
 						</Label>
 					</ReferenceLine>
 					<Bar dataKey="Wins" barSize={20} fill={primaryColor} yAxisId="right" />
-					<Line type="monotone" dataKey="Points Scored" stroke="#17b978" yAxisId="left" />
+					<Line type="monotone" dataKey="Points Per Game" stroke="#17b978" yAxisId="left" />
+					<Line type="monotone" dataKey="Points Against Per Game" stroke="red" yAxisId="left" />
 				</ComposedChart>
 			</ResponsiveContainer>
 		</Div>
